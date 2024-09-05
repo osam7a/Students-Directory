3cdaa8bba799174c29784d97d2af9ef3b5bb34ee
Osama Alhennawi <osamaziadalhinawi@gmail.com>
2024-09-05 23:58:01 +0400

    Basic Student View class & viewing template with theme

--- students/views.py
+++ students/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
 from django.views import View
+from django.shortcuts import get_object_or_404
+
+from students.models import Student
 
 # Create your views here.
+class StudentView(View):
+    def get(self, request, student_id):
+        student = get_object_or_404(Student, id=student_id)
+
+        return render(request, 'student-view.html', {
+            'student': student,
+        })
